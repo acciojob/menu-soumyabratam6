e64108411df1021f49962acb2faaf12e651a0f27
soumyabrata maity <soumyabratam6@gmail.com>
2025-03-12 21:56:38 +0530

    all file edit and fixed and update

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,7 +4,11 @@ const Menu = ({item}) => {
   return (
     <div className="menu-container">
       {item.map((item) => (
-        <div key={item.id} className="menu-item">
+        <div 
+        key={item.id} 
+        className="menu-item"
+        data-test-id={`menu-item-${item.category.toLowerCase()}`}
+        >
           <img src={item.img} alt={item.title} className="menu-item-img" />
           <div className="menu-item-details">
             <h4>{item.title}</h4>
